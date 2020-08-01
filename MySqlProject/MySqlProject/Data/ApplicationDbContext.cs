aaa7ff98a4334e2df9096e40fed8034710c85039
Rakib Hossain <nobelrakib03@gmail.com>
2020-08-01 21:47:24 +0600

    first commit

---
+++ MySqlProject/MySqlProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MySqlProject.Models;
+
+namespace MySqlProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+       // public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MySqlProject/MySqlProject/Data/TestContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySqlProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlProject.Data.Migrations
+{
+    public class TestContext : DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options)
+            :base(options)
+            
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(@"server=localhost;port=3306;database=db;uid=root;");
+            }
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //}
+        public DbSet<Product> Products { get; set; }
+    }
+}
